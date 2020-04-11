63d57cfe8bf56be40d8935766a04d527b65a9b6d
Michal Nowak <michnow363@student.polsl.pl>
2020-04-11 13:46:30 +0200

    Added separate tableview controller for static table with buttons

---
+++ ComputerNetworksConference/screens/sideMenu/SideMenuViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SideMenu.swift
+//  ComputerNetworksConference
+//
+//  Created by Michal on 4/11/20.
+//  Copyright © 2020 OtherCoders. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SideMenuViewController: UITableViewController {
+    
+}
