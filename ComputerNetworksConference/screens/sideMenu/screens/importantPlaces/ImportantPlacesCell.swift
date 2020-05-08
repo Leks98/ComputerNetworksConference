e35dea2be34ccfb4b06abd016c56c1b9de75ad8c
Michal Nowak <michnow363@student.polsl.pl>
2020-05-08 09:23:22 +0200

    Added important places cell with wrong constraints

---
+++ ComputerNetworksConference/screens/sideMenu/screens/importantPlaces/ImportantPlacesCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImportantPlacesCell.swift
+//  ComputerNetworksConference
+//
+//  Created by Michal on 5/7/20.
+//  Copyright © 2020 OtherCoders. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImportantPlacesCell: UITableViewCell {
+    
+}
