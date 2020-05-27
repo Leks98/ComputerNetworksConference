82c42aba3dc455c6b52208c61b1c2b0c6524517e
Michal Nowak <michnow363@student.polsl.pl>
2020-05-27 21:58:36 +0200

    Installed real swift

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'ComputerNetworksConference' do
+	pod 'RealmSwift', '5.0.0'
+end
---
+++ Pods/Target Support Files/Pods-ComputerNetworksConference/Pods-ComputerNetworksConference-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ComputerNetworksConference : NSObject
+@end
+@implementation PodsDummy_Pods_ComputerNetworksConference
+@end
